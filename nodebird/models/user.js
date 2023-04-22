8aaa3e6b71af127a3e9e5b32d0c3efa5e87022d8
HA JIN <w2128@e-mirim.hs.kr>
2023-04-22 18:31:12 +0900

    MySQL과 Sequelize로 데이터베이스 세팅(모델 생성)

---
+++ nodebird/models/user.js
@@ -0,0 +1,58 @@
+//MySQL과 시퀄라이즈로 데이터베이스 세팅하기
+const Sequelize = require("sequelize");
+//사용자 정보를 저장하는 모델
+class User extends Sequelize.Model {
+  static initiate(sequelize) {
+    User.init(
+      {
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+          unique: true,
+        },
+        nick: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        provider: {
+          type: Sequelize.ENUM("local", "kakao"),
+          allowNull: false,
+          defaultValue: "local",
+        },
+        snsId: {
+          type: Sequelize.STRING(30),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: "followingId",
+      as: "Followers",
+      through: "Follow",
+    });
+    db.User.belongsToMany(db.User, {
+      foreignKey: "followerId",
+      as: "Followings",
+      through: "Follow",
+    });
+  }
+}
+
+module.exports = User;
